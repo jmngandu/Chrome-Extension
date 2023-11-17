e8018d6cb8f46cc049a3077185050df4704150df
James Ngandu <jamesmngandu@gmail.com>
2023-11-17 20:52:24 +0300

    commit

--- Extension/index.js
+++ Extension/index.js
@@ -1,7 +1,13 @@
 let myLinks = [];
 const linkEl = document.querySelector('.link-el');
 const btnEL = document.querySelector('.btn-el');
-
+const listEl = document.querySelector('.list-el');
+console.log(listEl);
 btnEL.addEventListener('click', () => {
 	myLinks.push(linkEl.value);
+
+	for (let i = 0; i < myLinks.length; i++) {
+		console.log(myLinks[i]);
+		listEl.innerHTML += myLinks[i];
+	}
 });
