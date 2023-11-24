41e7e5786bca05c6c7c03cff01e545f1cdccd4e4
James Ngandu <jamesmngandu@gmail.com>
2023-11-25 02:15:36 +0300

    commit

--- Extension/index.js
+++ Extension/index.js
@@ -2,9 +2,8 @@ let myLinks = [];
 const linkEl = document.querySelector('.link-el');
 const btnEL = document.querySelector('.btn-el');
 const listEl = document.querySelector('.list-el');
-
 let linksFromLocalStorage = JSON.parse(localStorage.getItem('myLinks'));
-console.log(linksFromLocalStorage);
+
 if (linksFromLocalStorage) {
 	myLinks = linksFromLocalStorage;
 	renderMylinks();
