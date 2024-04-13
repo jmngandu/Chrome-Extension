3afb7aa8fef36d8a2fa01746afd0322f45c7a345
jmngandu <jamesmngandu@gmail.com>
2024-04-14 00:26:36 +0300

    commit

--- Extension/index.js
+++ Extension/index.js
@@ -1,51 +1,48 @@
 let myLinks = [];
-const linkEl = document.querySelector('.link-el');
-const btnEL = document.querySelector('.btn-el');
-const listEl = document.querySelector('.list-el');
-const btnDel = document.querySelector('.btn-del');
+const linkEl = document.querySelector(".link-el");
+const btnEL = document.querySelector(".btn-el");
+const listEl = document.querySelector(".list-el");
+const btnDel = document.querySelector(".btn-del");
 const btnSave = document.querySelector(".btn-saveTab");
 
-
-const linksFromLocalStorage = JSON.parse(localStorage.getItem('myLinks'));
+const linksFromLocalStorage = JSON.parse(localStorage.getItem("myLinks"));
 
 if (linksFromLocalStorage) {
-	myLinks = linksFromLocalStorage;
-	render(myLinks);
+  myLinks = linksFromLocalStorage;
+  render(myLinks);
 }
 
 function render(links) {
-	let theList = '';
-	for (let i = 0; i < links.length; i++) {
-		theList += `
+  let theList = "";
+  for (let i = 0; i < links.length; i++) {
+    theList += `
 				<li>
 					<a target="_blank"href="${links[i]}"> 
 					${links[i]} </a>
 				</li>`;
-	}
-	listEl.innerHTML = theList;
+  }
+  listEl.innerHTML = theList;
 }
 
-btnDel.addEventListener('dblclick', function () {
-	localStorage.clear();
-	myLinks = [];
-	render(myLinks);
+btnDel.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLinks = [];
+  render(myLinks);
 });
 
-btnEL.addEventListener('click', () => {
-	myLinks.push(linkEl.value);
-	linkEl.value = '';
+btnEL.addEventListener("click", () => {
+  myLinks.push(linkEl.value);
+  linkEl.value = "";
 
-	localStorage.setItem('myLinks', JSON.stringify(myLinks));
+  localStorage.setItem("myLinks", JSON.stringify(myLinks));
 
-	render(myLinks);
+  render(myLinks);
 });
 
-
-btnSave.addEventListener("click", ()=>{
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		myLinks.push(tabs[0].url)
-		localStorage.setItem('myLinks', JSON.stringify(myLinks));
-		render(myLinks);
-    })
-
-})
+btnSave.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLinks.push(tabs[0].url);
+    localStorage.setItem("myLinks", JSON.stringify(myLinks));
+    render(myLinks);
+  });
+});
